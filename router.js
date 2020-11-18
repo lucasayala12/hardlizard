ecac7db8d75b683bdee4fcb8343626bc0240bc75
Leo Terry <lucasenriquez1997@gmail.com>
2020-11-18 18:01:49 -0300

    Router finalizado

--- router.js
+++ router.js
@@ -8,8 +8,21 @@ module.exports = function(req,res){
             break;
         case '/en-cartelera':
             index.enCartelera(req,res)
-            break
+            break;
+        case "/sucursales":
+            index.sucursales(req,res)
+            break;
+        case "/preguntas-frecuentes":
+            index.pregntasFrecuentes(req,res)
+            break;
+        case "/mas-votadas":
+            index.masVotadas(req,res)
+            break;
+        case "/contacto":
+            index.contacto(req,res)
+            break;
         default:
+            res.write("404 not found")
             break;
     }
 }
