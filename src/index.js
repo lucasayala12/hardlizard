f9d1bf4f21316de46e061b8ef73ef35bfe4e9feb
Leo Terry <lucasenriquez1997@gmail.com>
2020-11-19 18:29:48 -0300

    Index sucursales y preguntas

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ let masVotadas = require('./masVotadas')
 let votadas = masVotadas.masVotadas()
 let enCartelera = require('./enCartelera')
 let preguntasFrecuentes = require("./preguntasFrecuentes")
+let sucursales = require("./sucursales")
+let teatros = sucursales.theaters()
 let peliculas = enCartelera.peliculas()
-let preguntas = preguntasFrecuentes.preguntas
+let preguntas = preguntasFrecuentes.preguntas()
 
 module.exports = {
     homePage : function(req,res){
@@ -31,7 +33,7 @@ module.exports = {
         res.write(`El total de peliculas es: ${masVotadas.total()}\n`)
         res.write(`El valoración general es: ${masVotadas.promedio()}\n\n`)
         votadas.forEach(peli => {
-            res.write(`-${peli.title}\nValoración: ${peli.vote_average}\n${peli.overview}\n\n`)
+            res.write(`${peli.title}\nValoración: ${peli.vote_average}\n${peli.overview}\n\n`)
         })
         res.end()
     },
@@ -39,10 +41,20 @@ module.exports = {
         
 
     },
-    preguntasFrecuentes : function (){
-        
+    preguntasFrecuentes : function (req,res){
+        res.write(`${preguntasFrecuentes.titulo}\n\n`)
+        res.write(`Total de preguntas: ${preguntasFrecuentes.total()}\n\n`)
+        preguntas.forEach(pregunta => {
+            res.write(`Pregunta: ${pregunta.faq_title}\n Respuesta: ${pregunta.faq_answer} \n\n`)
+        })
+        res.end()
     },
-    sucursales : function (){
-
+    sucursales : function (req,res){
+        res.write(`${sucursales.titulo}\n\n`)
+        res.write(`Contamos con ${sucursales.total()} sucursales\n\n`)
+        teatros.forEach(sucursal => {
+        res.write(`Sucursal: ${sucursal.name} \n Dirección: ${sucursal.addres} \n Descripción: ${sucursal.description} \n Total de salas: ${sucursal.total_rooms}\n\n`)
+        })
+        res.end()
     }
 }
